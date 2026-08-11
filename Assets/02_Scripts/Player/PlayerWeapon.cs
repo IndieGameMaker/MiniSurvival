bbce0094a8177b7d246c6fc6370f8690e8cab959
Lee JaeHyun <myleje@gmail.com>
2026-08-11 14:04:44 +0900

    feat: add PlayerWeapon class for player shooting mechanics and enemy targeting

---
+++ Assets/02_Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private float _fireRate = 0.5f; // 발사간격
+    
+    
+    private float _nextFire;
+
+    private void Update()
+    {
+        // 발사 쿨다운
+        _nextFire += Time.deltaTime;
+        if (_nextFire < _fireRate) return;
+        
+        // 가장 가까운 적 검색
+        
+        _nextFire = 0;
+    }
+
+    private Transform FindEnemy()
+    {
+        EnemyHealth[] enemies = FindObjectsByType<EnemyHealth>(FindObjectsSortMode.None);
+
+        Transform nearest = null;
+        float nearestDist = 0f;
+        foreach (var enemy in enemies)
+        {
+            // 거리 비교 (두좌표간의 거리) Vector3.Distance (A, B)
+            float dist = (enemy.transform.position - transform.position).sqrMagnitude;
+            if (dist < nearestDist)
+            {
+                nearestDist = dist;
+                nearest = enemy.transform;
+            }
+        }
+        
+        return nearest;
+    }
+}
