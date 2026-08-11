ec45b975c54b0b764d3d76ba194933d8ecd38c66
Lee JaeHyun <myleje@gmail.com>
2026-08-11 11:33:32 +0900

    feat: integrate new input system for player movement

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,5 +7,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 5.0f;
     [Range(90f, 1440f)]
     [SerializeField] private float _turnSpeed = 720.0f;
+    
+    private InputAction _moveAction;
 
+    private void OnEnable()
+    {
+        // Move 액션 검색 및 할당
+        _moveAction = InputSystem.actions.FindAction("Move");
+        // 액션 활성화
+        _moveAction.Enable();
+    }
 }
