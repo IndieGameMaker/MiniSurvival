25eab41a0067aa44609f5404a841329523d774fc
Lee JaeHyun <myleje@gmail.com>
2026-08-11 20:53:45 +0900

    feat: add damage property to Bullet class and update damage handling

--- Assets/02_Scripts/Player/Bullet.cs
+++ Assets/02_Scripts/Player/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
+    [SerializeField] private int _damage = 25;
 
     private void Update()
     {
@@ -15,7 +16,7 @@ public class Bullet : MonoBehaviour
         // 충돌 대상이 IDamageable 인터페이스를 구현한 것이면 데미지 처리
         if (other.TryGetComponent<IDamageable>(out IDamageable enemy))
         {
-            enemy.TakeDamage(25);
+            enemy.TakeDamage(_damage);
             Destroy(gameObject);
         }
     }
