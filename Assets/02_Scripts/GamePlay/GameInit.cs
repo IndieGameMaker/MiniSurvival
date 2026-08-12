7a2888bd4fba17ddf08e7db60495fbdc770d9821
Lee JaeHyun <myleje@gmail.com>
2026-08-12 16:25:13 +0900

    feat: add GameInit class for asynchronous game initialization and loading tasks

---
+++ Assets/02_Scripts/GamePlay/GameInit.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour
+{
+    // Task.WhenAll
+
+    private async void Start()
+    {
+        Debug.Log("게임 초기화 시작 ...");
+
+        Task task1 = LoadSaveDataAsync();
+        Task task2 = LoadGameConfigAsync();
+        Task task3 = SetObjectPoolAsync();
+    }
+
+    private async Task LoadSaveDataAsync()
+    {
+        await Task.Delay(1000);
+        Debug.Log("[Task #1] 세이브 데이터 로딩 완료");
+    }
+    
+    private async Task LoadGameConfigAsync()
+    {
+        await Task.Delay(2000);
+        Debug.Log("[Task #2] 게임 설정 데이터 로딩 완료");
+    }  
+    
+    private async Task SetObjectPoolAsync()
+    {
+        await Task.Delay(500);
+        Debug.Log("[Task #3] 오브젝트풀링 초기화 완료");
+    }     
+}
