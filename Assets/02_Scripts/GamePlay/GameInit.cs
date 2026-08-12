97309488cba22921ff25773b0ad3a5dcbb32a543
Lee JaeHyun <myleje@gmail.com>
2026-08-12 16:42:48 +0900

    feat: improve GameInit for better asynchronous task handling and error management

--- Assets/02_Scripts/GamePlay/GameInit.cs
+++ Assets/02_Scripts/GamePlay/GameInit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -12,10 +13,14 @@ public class GameInit : MonoBehaviour
         Task task1 = LoadSaveDataAsync();
         Task task2 = LoadGameConfigAsync();
         Task task3 = SetObjectPoolAsync();
-        
+
         // Task.WhenAll 동시에 작업을 시작하고 비동기로 대기
         await Task.WhenAll(task1, task2, task3);
-        
+
+        // await LoadSaveDataAsync();
+        // await SetObjectPoolAsync();
+        // await LoadGameConfigAsync();
+
         Debug.Log("초기화 완료!");
     }
 
@@ -24,16 +29,39 @@ public class GameInit : MonoBehaviour
         await Task.Delay(1000);
         Debug.Log("[Task #1] 세이브 데이터 로딩 완료");
     }
-    
+
     private async Task LoadGameConfigAsync()
     {
         await Task.Delay(2000);
         Debug.Log("[Task #2] 게임 설정 데이터 로딩 완료");
-    }  
-    
+    }
+
     private async Task SetObjectPoolAsync()
     {
-        await Task.Delay(500);
-        Debug.Log("[Task #3] 오브젝트풀링 초기화 완료");
-    }     
+        try
+        {
+            await Task.Delay(500);
+            Debug.Log("[Task #3] 오브젝트풀링 초기화 완료");
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
 }
+
+/*
+ * Unity 6.5 지원 Awaitable
+ * 유니티 표준 비동기 기능
+ * async / await 문법 사용
+ * Task 의 불편한 점을 개선 (힙에 할당)
+ * 반환타입 Awaitable ,  Awaitable<T>  ==> Task 대체
+ * try-catch 가능
+ * 프레임 단위로 대기
+ * 풀링 기법을 사용 부담 적다. (재사용 가능)
+ *
+ * Awaitable.NextFrameAsync()   => yield return null
+ * Awaitable.WaitForSecondsAsync(2f)
+ * Awaitable.FixedUpdateAsync()
+ * Awaitable.MainTh readAsync() => 메인스레드로 복귀
+ */
