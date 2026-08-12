ba8d826e83013ec0f4753e4fc1a3394bf2889c55
Lee JaeHyun <myleje@gmail.com>
2026-08-12 13:51:03 +0900

    feat: implement player respawn coroutine in WaveManager and log respawn events

--- Assets/02_Scripts/GamePlay/WaveManager.cs
+++ Assets/02_Scripts/GamePlay/WaveManager.cs
@@ -1,16 +1,32 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
+
+/* 코루틴 (Co-routine)
+ * 
+ *
+ * 
+ */
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        StartCoroutine(RespawnPlayer());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.Log(Time.time);
     }
+    
+    private IEnumerator RespawnPlayer()
+    {
+        Debug.Log("리스폰 시작 : 3초 후에 부활");
+        yield return new WaitForSeconds(3);
+        // Thread.Sleep(3000); // 밀리세컨즈 3초, Blocking 방식
+        Debug.Log("플레이어 리스폰 완료");
+    }
+    
+    
 }
