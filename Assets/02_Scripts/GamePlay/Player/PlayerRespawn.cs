217895e500fa8610c97a14ea236ca7401416d749
Lee JaeHyun <myleje@gmail.com>
2026-08-12 14:37:14 +0900

    feat: add PlayerRespawn class with respawn delay and god mode functionality

---
+++ Assets/02_Scripts/GamePlay/Player/PlayerRespawn.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [SerializeField] private float _respawnDelay = 3f; // 부활 대기 시간
+    [SerializeField] private float _godModeTime = 2f; // 무적 모드 시간
+
+    [SerializeField] private bool _isGodMode; // 무적 여부
+
+    private WaitForSeconds _wait;
+    
+    private void Start()
+    {
+        _wait = new WaitForSeconds(_respawnDelay);
+    }
+    
+    public void BeginRespawn()
+    {
+        
+    }
+
+    private IEnumerator Respawn()
+    {
+        Debug.Log("주인공 사망 - 부활 대기중...");
+        
+        // 3초 대기
+        yield return _wait;
+    }
+}
