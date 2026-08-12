347baf0df8f15493d00a39ab9bd5f7d4e9a92cf6
Lee JaeHyun <myleje@gmail.com>
2026-08-12 13:55:22 +0900

    feat: deactivate enemy game objects in GamePlay and update WaveManager comments

--- Assets/02_Scripts/GamePlay/WaveManager.cs
+++ Assets/02_Scripts/GamePlay/WaveManager.cs
@@ -20,6 +20,8 @@ public class WaveManager : MonoBehaviour
         Debug.Log(Time.time);
     }
     
+    // 코루틴은 병렬처리를 위한 유니티 엔진 자체의 방법
+    // 멀티스레가 아님, 싱글 스레드 에서 동작
     private IEnumerator RespawnPlayer()
     {
         Debug.Log("리스폰 시작 : 3초 후에 부활");
