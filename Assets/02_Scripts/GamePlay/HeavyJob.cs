8c132da9b5f98c673de668138826bc734516ce84
Lee JaeHyun <myleje@gmail.com>
2026-08-13 10:35:16 +0900

    feat: add stopwatch for performance measurement in HeavyJob calculations

--- Assets/02_Scripts/GamePlay/HeavyJob.cs
+++ Assets/02_Scripts/GamePlay/HeavyJob.cs
@@ -7,10 +7,14 @@ public class HeavyJob : MonoBehaviour
 
     private async Awaitable Start()
     {
+        // 스탑워치 객체 생성
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        
         // 메인 스레드 ID 추출
         _mainThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
         Debug.Log($"시작 스레드가 메인? {IsMainThread()}");
         
+        stopwatch.Start();
         // 백그라운드 스레드로 전환
         await Awaitable.BackgroundThreadAsync();
         long sum = HeavyCalc();
@@ -18,8 +22,9 @@ public class HeavyJob : MonoBehaviour
         
         // 메인스레드로 전환
         await Awaitable.MainThreadAsync();
+        stopwatch.Stop();
         // transform.position = new Vector3((float)sum / 2, (float)sum / 2, 0);
-        Debug.Log($"결과값 {sum}");
+        Debug.Log($"경과시간 {stopwatch.ElapsedMilliseconds} 결과값 {sum}");
     }
 
     private long HeavyCalc()
